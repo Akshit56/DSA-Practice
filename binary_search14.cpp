128f56a76c51b7a182b2fde4ced3a30f68168a6f
Akshit Gupta <akshit0689@gmail.com>
2023-11-02 22:09:51 +0530

    BS to find pairs with a particular sum

---
+++ binary_search14.cpp
@@ -0,0 +1,17 @@
+// To find a pair with a specific sum value (microsoft,flipkart)
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+  int sum = 5;
+  vector<int> arr{1, 3, 5, 7, 9};
+  vector<int> brr{2, 4, 6, 8};
+  for (int i = 0; i < arr.size(); i++) {
+    for (int j = 0; j < brr.size(); j++) {
+      if (arr[i] + brr[j] == sum) {
+        cout << arr[i] << "," << brr[j] << endl;
+      }
+    }
+  }
+  return 0;
+}
