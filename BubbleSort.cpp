5025a3cca2a7b0880e000367d066577803004415
Akshit Gupta <akshit0689@gmail.com>
2023-11-15 22:49:56 +0530

    Bubble Sort

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -13,7 +13,7 @@ void bubbleSort(vector<int> &v){
     int n = v.size();
     for(int i=0; i<n; i++){
         for(int j=0; j<n-1-i; j++){
-            if(v[j] > v[j+1]){
+            if(v[j] > v[j+1]){         // v[j] < v[j+1] if sorting in desc order
                 swap(v[j] , v[j+1]);
             }
         }
