99aad3474545deb2ef0a6cc3732519b261228a63
Akshit Gupta <akshit0689@gmail.com>
2023-11-21 23:07:55 +0530

    Sum of Digits

---
+++ sum_of_digits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number,digit;
+    cout<<"Enter the number :"<<endl;
+    cin>>number;
+    int sum = 0;
+    while(number !=0){
+        digit = number%10;
+        number = number/10;
+        sum = sum+digit;
+    }
+    cout<<"The sum of digits are : "<<sum;
+    return sum;
+}
