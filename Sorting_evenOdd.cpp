ebcc1b244d0f0126ab3fd5674b58c99694985878
Akshit Gupta <akshit0689@gmail.com>
2023-11-22 23:47:56 +0530

    Sorting even & odd

---
+++ Sorting_evenOdd.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void sortEvenOdd(vector<int> &v){
+    int leftptr=0, rightptr=v.size()-1;
+    
+    while(leftptr<rightptr){
+        if(v[leftptr]%2==1 && v[rightptr]&2==0){
+            swap(v[leftptr],v[rightptr]);
+            leftptr++; rightptr--;
+        }
+        if(v[leftptr]%2==0){
+            leftptr++;
+        }
+        if(v[rightptr]%2==1){
+            rightptr--;
+        }
+    }
+    return;
+}
+
+int main(){
+    
+    int n; 
+    cin>>n;
+    vector<int> v;
+    for (int i=0; i<n; i++){
+        int ele; cin>>ele;
+        v.push_back(ele);
+    }
+    
+    sortEvenOdd(v);
+    for(int i=0; i<n; i++){
+        cout<<v[i]<<" ";
+    }
+    return 0;
+    
+}
