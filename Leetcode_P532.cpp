19fdcae50e5c1f9c3c64b42bf4ecdb9a60ed40e8
Akshit Gupta <akshit0689@gmail.com>
2023-11-30 23:53:01 +0530

    Leetcode Problem 532

---
+++ Leetcode_P532.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include <bits/stdc++.h>
+#include<set>
+using namespace std;
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        int i=0,j=1;
+        set<pair<int,int>> ans;
+        sort(nums.begin(),nums.end());
+        while(j<nums.size()){
+            int diff = nums[j]-nums[i];
+            if(diff == k){
+                ans.insert({nums[i],nums[j]});
+                ++i, ++j;
+                
+            }
+            else if(diff > k){
+                i++;
+            }
+            else{
+                j++;
+            }
+            if(i==j) j++;
+        }
+       return ans.size(); 
+    }
+};
+
+int main(){
+
+
+    vector<int>nums;
+    for(int i=0; i<nums.size(); i++){
+        nums.push_back(nums[i]);
+    }
+    int k;
+    cin>>k;
+    Solution pairs;
+    pairs.findPairs(nums,k);
+    return 0;
+}
