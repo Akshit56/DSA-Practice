4237e3ce3d45cd94e1b71a69501c120e05f681c9
Akshit Gupta <akshit0689@gmail.com>
2023-12-10 23:29:17 +0530

    removal of symbols from char array

---
+++ char_strings3.cpp
@@ -0,0 +1,24 @@
+// Remove symbols '@' from character string
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+void changeDigit(char ch[], int size) {
+
+  int index = 0;
+  while (ch[index] != '\0') {
+    if (ch[index] == '@') {
+      ch[index] = ch[index] - '@' + ' ';
+    }
+    index++;
+  }
+}
+
+int main() {
+  char ch[100];
+  cin.getline(ch, 100);
+  cout << "Before : " << ch << endl;
+  changeDigit(ch, 100);
+  cout << "After : " << ch;
+  return 0;
+}
