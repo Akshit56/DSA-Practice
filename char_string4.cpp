3601e8f7797ab9f6642894d154916fae580b1175
Akshit Gupta <akshit0689@gmail.com>
2023-12-11 23:11:05 +0530

    reversing a string

---
+++ char_string4.cpp
@@ -0,0 +1,35 @@
+// Reverse a character string
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int stringLength(char ch[]) {
+
+  int index = 0;
+  while (ch[index] != '\0') {
+    index++;
+  }
+  return index;
+}
+
+void revChar(char ch[], int n) {
+
+  int l = 0;
+  int h = n - 1;
+  while (h >= l) {
+    swap(ch[l], ch[h]);
+    l++;
+    h--;
+  }
+}
+
+int main() {
+
+  char ch[100];
+  cin.getline(ch, 100);
+  cout << "Before : " << ch << endl;
+  revChar(ch, stringLength(ch));
+  cout << "After : " << ch;
+
+  return 0;
+}
