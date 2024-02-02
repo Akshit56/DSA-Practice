563fd29e4fa063d1ae4ba3a56d510c6ff5a805c9
Akshit Gupta <akshit0689@gmail.com>
2024-02-02 13:13:45 +0530

    Binary Search using recursion

--- Recursion/BinarySearchUsingRecursion.cpp
+++ Recursion/BinarySearchUsingRecursion.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int binSearch(int arr, int size, int target, int start, int end)
+int binSearch(int arr[], int size, int target, int start, int end)
 {
 
   int mid = start + (end - start) / 2;
@@ -9,15 +9,40 @@ int binSearch(int arr, int size, int target, int start, int end)
   {
     return -1;
   }
-}
+
+  if (target == arr[mid])
+  {
+    return mid;
+  }
+
+  if (target < arr[mid])
+  {
+    end = mid - 1;
+    return binSearch(arr, size, target, start, end);
+  }
+
+  if (target > arr[mid])
+  {
+    start = mid + 1;
+    return binSearch(arr, size, target, start, end);
+  }
+};
 
 int main()
 {
 
   int arr[] = {10, 20, 30, 40, 50, 60, 70, 80};
   int size = 8;
-  int target = 40;
+  int target = 80;
   int start = 0;
   int end = size - 1;
   int ans = binSearch(arr, size, target, start, end);
+  if (ans != -1)
+  {
+    cout << "Found at : " << ans;
+  }
+  else
+  {
+    cout << "Not found";
+  }
 }
