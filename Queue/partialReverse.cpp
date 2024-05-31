f2cad873dcaf995542dc43215634f5750472b9d6
Akshit Gupta <akshit0689@gmail.com>
2024-05-31 16:29:21 +0530

    Partial reverse of a queue using stack in C++

---
+++ Queue/partialReverse.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+
+int main()
+{
+  int k;
+  cout << "Enter how many first numbers you want to reverse : ";
+  cin >> k;
+  stack<int> st;
+  queue<int> q;
+  q.push(10);
+  q.push(20);
+  q.push(30);
+  q.push(40);
+  q.push(50);
+  // q.push(60);
+
+  for (int i = 0; i < k; i++)
+  {
+    int temp = q.front();
+    q.pop();
+    st.push(temp);
+  }
+
+  while (!st.empty())
+  {
+    int temp = st.top();
+    st.pop();
+    q.push(temp);
+  }
+
+  for (int i = 0; i < q.size() - k; i++)
+  {
+    int temp = q.front();
+    q.pop();
+    q.push(temp);
+  }
+
+  while (!q.empty())
+  {
+    int temp = q.front();
+    q.pop();
+    cout << temp << " ";
+  }
+}
