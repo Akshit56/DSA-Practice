9960fcc51d226f0b169aa4f407b2f8cc51df34cd
Akshit Gupta <akshit0689@gmail.com>
2024-07-06 12:33:13 +0530

    refactoring

--- char_strings3.cpp
+++ char_strings3.cpp
@@ -3,18 +3,22 @@
 #include <string.h>
 using namespace std;
 
-void changeDigit(char ch[], int size) {
+void changeDigit(char ch[], int size)
+{
 
   int index = 0;
-  while (ch[index] != '\0') {
-    if (ch[index] == '@') {
+  while (ch[index] != '\0')
+  {
+    if (ch[index] == '@')
+    {
       ch[index] = ch[index] - '@' + ' ';
     }
     index++;
   }
 }
 
-int main() {
+int main()
+{
   char ch[100];
   cin.getline(ch, 100);
   cout << "Before : " << ch << endl;
