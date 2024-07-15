b9c20c792ac3fa7f0ccd99d2d4f3031dedd66cb7
Akshit Gupta <akshit0689@gmail.com>
2024-07-15 12:57:28 +0530

    Move all negative numbers to the left side of the array

---
+++ Questions/Arrays/MoveNegative.cpp
@@ -0,0 +1,39 @@
+// Move all the negative numbers to the left of the given array
+
+#include <iostream>
+using namespace std;
+// Durtch Flag Algorithm (basically 2 pointer approach)
+// TC - O(n), SC - O(1)
+void moveNegative(int *a, int n)
+{
+  int l = 0;
+  int h = n - 1;
+  while (l < h)
+  {
+    if (a[l] < 0)
+    {
+      l++;
+    }
+    else if (a[h] > 0)
+    {
+      h--;
+    }
+    else
+    {
+      swap(a[l], a[h]);
+    }
+  }
+}
+
+int main()
+{
+  int a[] = {1, 3, -5, 8, -9, 10};
+  int n = sizeof(a) / sizeof(a[0]);
+  moveNegative(a, n);
+
+  for (int i = 0; i < n; i++)
+  {
+    cout << a[i] << " ";
+  }
+  return 0;
+}
